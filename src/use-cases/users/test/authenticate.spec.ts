799ad2549422c4f1d009647090cd2f03919274b7
carlosgodoi <edu-godoi@hotmail.com>
2026-07-20 12:11:49 -0300

    test: Aplicando testes para register e authenticate users

---
+++ src/use-cases/users/test/authenticate.spec.ts
@@ -0,0 +1,61 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { AuthenticateUseCase } from '../authenticate';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { hash } from 'bcrypt';
+import { AppError } from '@/utils/errors/appError';
+
+let usersRpository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRpository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRpository);
+  });
+
+  it('Shoulde be able to authenticate', async () => {
+    await usersRpository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      phone: '55 99988-9809',
+      profile: 'VISITOR',
+      situation: 'ACTIVE',
+      password: await hash('Test@123', 8),
+      createdAt: new Date(),
+    });
+
+    const { user } = await sut.execute({
+      email: 'johndoe@mail.com',
+      password: 'Test@123',
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('Shoulde not be able to authenticate with wrong email', async () => {
+    await expect(
+      sut.execute({
+        email: 'johndoe@mail.com',
+        password: 'Test@123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to authenticate with wrong password', async () => {
+    await usersRpository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      phone: '55 99988-9809',
+      profile: 'VISITOR',
+      situation: 'ACTIVE',
+      password: await hash('Test@123', 8),
+    });
+
+    expect(() => {
+      sut.execute({
+        email: 'johndoe@mail.com',
+        password: '123123',
+      });
+    });
+  });
+});
---
+++ src/use-cases/users/test/register.spec.ts
@@ -0,0 +1,66 @@
+import { compare } from 'bcrypt';
+import { InMemoryUsersRepository } from '../../../repositories/in-memory/in-memory-users-repository';
+import { RegisterUserUserCase } from '../../users/register';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { AppError } from '../../../utils/errors/appError';
+
+let usersRepository: InMemoryUsersRepository;
+let sut: RegisterUserUserCase;
+
+describe('Register Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new RegisterUserUserCase(usersRepository);
+  });
+
+  it('Shoulde be able to register', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      phone: '55 99988-9809',
+      profile: 'VISITOR',
+      situation: 'ACTIVE',
+      password: 'Test@123',
+    });
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('Should hash user password upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      phone: '55 99988-9809',
+      profile: 'VISITOR',
+      situation: 'ACTIVE',
+      password: 'Test@123',
+    });
+
+    const isPasswordCorrectlyHashed = await compare('Test@123', user.password);
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it('Should not be able to register with same email twice', async () => {
+    const email = 'johndoe@mail.com';
+
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      phone: '55 99988-9809',
+      profile: 'VISITOR',
+      situation: 'ACTIVE',
+      password: 'Test@123',
+    });
+
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email,
+        phone: '55 99988-9809',
+        profile: 'VISITOR',
+        situation: 'ACTIVE',
+        password: 'Test@123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
